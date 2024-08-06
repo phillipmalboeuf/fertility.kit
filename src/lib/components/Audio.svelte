c9f8ed2649337332c39f5d43c78c3f50b4bd7182
Phillip Malboeuf <phil@phils.computer>
2024-08-06 10:13:27 -0400

    play icons

--- src/lib/components/Audio.svelte
+++ src/lib/components/Audio.svelte
@@ -40,11 +40,14 @@
   {:else}
   {media.fields.title}
   {#if paused}
-  <svg width="18" height="20" viewBox="0 0 18 20"> <path d="M1.64229 19.4947C0.822254 19.4947 0.3125 18.8554 0.3125 17.8795V2.1092C0.3125 1.13337 0.822254 0.505249 1.64229 0.505249C2.08556 0.505249 2.46233 0.673496 2.9056 0.931474L15.8268 8.50258C16.7465 9.04097 17.0679 9.39989 17.0679 9.99436C17.0679 10.5888 16.7465 10.9478 15.8268 11.4974L2.9056 19.0573C2.46233 19.3152 2.08556 19.4947 1.64229 19.4947Z" fill="currentColor"/> </svg>
+  <svg width="14" height="14" viewBox="0 0 14 14">
+    <path d="M9.83333 6.9L5 4V9.8L9.83333 6.9Z" fill="currentColor"/>
+    <circle cx="7" cy="7" r="6.5" stroke="currentColor"/>
+  </svg>
   {:else}
-  <svg width="18" height="20" viewBox="0 0 17 21">
-  <rect x="5.80566" y="0.192383" width="19.9995" height="5.28146" rx="2.64073" transform="rotate(90 5.80566 0.192383)" fill="currentColor"/>
-  <rect x="16.0869" y="0.192383" width="19.9995" height="5.28146" rx="2.64073" transform="rotate(90 16.0869 0.192383)" fill="currentColor"/>
+  <svg width="14" height="14" viewBox="0 0 15 15">
+  <circle cx="7.5" cy="7.5" r="7" stroke="currentColor"/>
+  <rect x="5" y="5" width="5" height="5" fill="currentColor"/>
   </svg>
   {/if}
   {/if}
